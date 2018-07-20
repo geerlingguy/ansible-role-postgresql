f2ea1ed7247f228fcac907c2546c956b3dabca49
Alexey Churikov <a.churikov@voskhod.ru>
2018-07-20 16:12:38 +0300

    Fix travis file

--- tests/alexgluck.test.sh
+++ tests/alexgluck.test.sh
@@ -13,13 +13,11 @@ for OSTYPE in "$role_dir"/vars/* ; do
       continue
     fi
     OS_PSQL_START_VERSION="$(grep -i "$OS" "$role_dir"/tasks/install_official_repo.yml |tail -c 5 | cut -f1 -d '+')"
-    #echo -e "distro=$OS"
     for POSTGRES_VERSION in ${POSTGRES_VERSION_LIST}; do
       if [[ "$(echo "(${POSTGRES_VERSION}*10)/1" | bc)" -lt "$(echo "(${OS_PSQL_START_VERSION}*10)/1" | bc)" ]]; then
         continue
       fi
-      echo -e "- distro: $OS\n  postgresql_version: $POSTGRES_VERSION\n"
-      #distro="$OS"  extra_vars="-e postgresql_version=$POSTGRES_VERSION -e install_official_repo=true -v" "$(dirname "$0")"/test.sh >"$role_dir"/"$OS"-"$POSTGRES_VERSION".log 2>&1 &
+      echo -e "- distro: $OS  postgresql_version: $POSTGRES_VERSION"
     done
   done
 done
